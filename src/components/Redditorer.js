3feb871876b50f2d539b94f33a9144e2d566f4f2
WitmerDesigns <patrick@witmerdesigns.com>
2020-02-04 12:22:18 -0700

    updated commit

--- src/components/Redditorer.js
+++ src/components/Redditorer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SubReddit from './SubReddit';
+// import SubReddit from './SubReddit';
 import UpDoots from './UpDoots';
 import styled from 'styled-components';
 
@@ -34,18 +34,39 @@ const CommentsNum = styled.button`
   margin: 50px 50px 0 50px;
 `;
 
+const Subreddit = styled.input`
+  display: block;
+  width: 80%;
+  height: 50px;
+  padding: 10px;
+  text-align: center;
+`;
+
+// ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~
+//                      Styling End
+// ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~
+
 class Redditorer extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.url = 'https://www.reddit.com/r/';
     this.state = {
       error: null,
       isLoaded: false,
       items: [],
-      subReddit: ''
+      subReddit: 'blackmetal'
     };
   }
-  async loadReddit() {
-    await fetch(`https://www.reddit.com/r/${this.state.subReddit}` + '.json')
+  loadReddit(sub) {
+    console.log(sub);
+    this.setState({
+      items: [],
+      subReddit: sub
+    });
+
+    fetch(this.url + sub + '.json')
       .then(res => res.json())
       .then(
         result => {
@@ -55,9 +76,7 @@ class Redditorer extends React.Component {
             // subReddit: this.state.subReddit
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+
         error => {
           this.setState({
             isLoaded: true,
@@ -68,7 +87,21 @@ class Redditorer extends React.Component {
   }
 
   componentDidMount() {
-    this.loadReddit();
+    this.loadReddit(this.state.subReddit);
+  }
+
+  // componentDidUpdate() {
+  //   this.loadReddit();
+  // }
+
+  handleChange(e) {
+    this.setState({ subReddit: e.target.value });
+    console.log(e);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.loadReddit(e);
   }
 
   render() {
@@ -77,31 +110,43 @@ class Redditorer extends React.Component {
     //   return <div>Error: {error.message}</div>;
     // } else
 
-    if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <>
-          <SubReddit subreddit={this.state.subReddit} />
+    // if (!isLoaded) {
+    //   return <div>Loading...</div>;
+    // } else {
+    return (
+      <>
+        {/* <SubReddit subreddit={this.state.subReddit} /> */}
 
-          <RedditMain>
-            {items.map((item, i) => (
-              <RedditCard key={i}>
-                <img src={item.data.thumbnail} alt="Related Post" />
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter a Subreddit:</label>
+          <Subreddit
+            type="text"
+            onChange={this.handleChange}
+            // onChange={e => this.search(e.target.value)}
+            // onInput={loadReddit()}
+            value="SHITSHITSHIT"
+            // placeholder="subreddit"
+          />
+          <input type="submit" value="Submit" />
+        </form>
 
-                <UpDoots score={item.data.score} />
+        <RedditMain>
+          {items.map((item, i) => (
+            <RedditCard key={i}>
+              <img src={item.data.thumbnail} alt="Related Post" />
 
-                <TitleLink href={'https://reddit.com/' + item.data.permalink}>
-                  {item.data.title}
-                </TitleLink>
+              <UpDoots score={item.data.score} />
 
-                <CommentsNum>Comments: {item.data.num_comments}</CommentsNum>
-              </RedditCard>
-            ))}
-          </RedditMain>
-        </>
-      );
-    }
+              <TitleLink href={'https://reddit.com/' + item.data.permalink}>
+                {item.data.title}
+              </TitleLink>
+
+              <CommentsNum>Comments: {item.data.num_comments}</CommentsNum>
+            </RedditCard>
+          ))}
+        </RedditMain>
+      </>
+    );
   }
 }
 
--- src/components/SubReddit.js
+++ src/components/SubReddit.js
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { render } from '@testing-library/react';
 // import { loadReddit } from './Redditorer';
-
-const Subreddit = styled.input`
-  display: block;
-  width: 80%;
-  height: 50px;
-  padding: 10px;
-  text-align: center;
-`;
 
 const SubReddit = props => {
   const [newsub, setSub] = useState(props.subreddit);
@@ -24,7 +17,7 @@ const SubReddit = props => {
       <Subreddit
         type="text"
         // onChange={e => setSub(e.target.value)}
-        onChange={handleChange}
+        onChange={loadReddit}
         // onInput={loadReddit()}
         value={newsub}
         name="search"
